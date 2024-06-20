76339335cd47134effc9cc9b4d596e37848db38a
supitsara22 <supitsara.kc@gmail.com>
2024-06-20 02:03:47 +0000

    first

--- myfirst.js
+++ myfirst.js
@@ -1,6 +1,6 @@
 let http = require('http');
 
-hiip.createServer(function (req, res) {
+http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('Hello World');
 }).listen(9000);
